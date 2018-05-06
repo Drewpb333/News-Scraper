b6047422693857840df7ab72f8eadc93eff661a7
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-05-06 16:17:10 -0400

    changes to news-scraper template

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ var cheerio = require("cheerio");
 var db = require("./models");
 
 var app = express();
+
+// Connects to heroku first if possible
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
